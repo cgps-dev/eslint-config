431b173856cba283f56d8d091513636e0b4384fa
Khalil <ka10@sanger.ac.uk>
2020-08-14 14:41:06 +0100

    Create vue.js

---
+++ vue.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": [
+    "airbnb-base",
+    "plugin:vue/strongly-recommended",
+  ],
+  "rules": {
+    "vue/attributes-order": [ "error" ],
+    "vue/order-in-components": [ "error" ],
+    "vue/require-default-prop": 0,
+    "vue/v-bind-style": [ "error", "longform" ],
+    "vue/v-on-style": [ "error", "longform" ],
+  },
+};
