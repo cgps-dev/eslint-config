0d6592e0104b344e79ced3afaab98003938c4e4f
Khalil <ka10@sanger.ac.uk>
2020-08-14 14:41:27 +0100

    Create react.js

---
+++ react.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "extends": [
+    "airbnb-base",
+    "plugin:react",
+  ],
+  "rules": {
+    "react/prefer-es6-class": 0,
+    "react/prefer-stateless-function": 0,
+  },
+};
